774d8beaa8660b44a1c7828475b1a9e30f353e10
jay16 <jay_li@intfocus.com>
2017-03-28 13:27:48 +0800

    init

---
+++ json.rb
@@ -0,0 +1,16 @@
+fields = <<-EOF
+FACCOUNTNO
+FMOBILEPHONE
+FEMAILADR
+FNAME
+EOF
+
+words = fields.split(/\n/).map(&:strip)
+str = words.map do |word|
+  "    #{word.downcase},"
+end.join("\n")
+puts "var #{str};"
+str = words.map do |word|
+  "&quot;#{word}&quot;: &quot;' + #{word.downcase} + '&quot;"
+end.join(',')
+puts %Q(var params = '{#{str}}';)
---
+++ public/assets/js/remJs.js
@@ -0,0 +1,21 @@
+/**
+ * 2016/3/19 */
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            var font = 20 * (clientWidth / 320) < 40 ? 20 * (clientWidth / 320) : 40;
+            docEl.style.fontSize = font + 'px';
+
+            if(font < 30){
+                $('.tgl').hide();
+            }else{
+                $('.tgl').show();
+            }
+        };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
