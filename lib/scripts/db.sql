0a0a5829e65fa58b35204f0c99f28a87e2c527d4
jay16 <jay_li@intfocus.com>
2017-04-13 16:52:32 +0800

    optimized(ipad): style/images

--- lib/scripts/db.sql
+++ lib/scripts/db.sql
@@ -1,2 +1,11 @@
 CREATE DATABASE IF NOT EXISTS redeem_production DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
 insert into sys_model_0(field0, field1, field2, field5) values('admin', 'admin', 'admin@tkh.com', 'yes')
+
+
+window.onerror = function(errorMessage, scriptURI, lineNumber,columnNumber,errorObj) {
+ alert("错误信息：" + errorMessage);
+ alert("出错文件：" + scriptURI);
+ alert("出错行号：" + lineNumber);
+ alert("出错列号：" + columnNumber);
+ alert("错误详情：" + errorObj);
+}
